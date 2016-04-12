bbbacdca302c95be1cfa2d12c6b866148cf2b209
Ryan Batty <rgbatty@outlook.com>
2016-04-11 18:28:23 -0600

    Refactored basic server

--- lib/server.rb
+++ lib/server.rb
@@ -1,33 +1,45 @@
 class Server
   require 'socket'
-  tcp_server = TCPServer.new(9292)
-  num_runs = 0
 
-  loop do
-    client = tcp_server.accept
-    num_runs += 1
-    puts "Ready for a request"
+  attr_accessor :running, :tcp_server
+
+  def initialize
+    @running = true
+  end
+
+  def start
+    num_runs = 0
+    tcp_server = TCPServer.new(9292)
+    while running? do
+      client = tcp_server.accept
+      num_runs += 1
+
+      client.puts generate_response(get_request(client), "Hello World! #{num_runs/2}")
+      client.close
+    end
+  end
+
+  def running?
+    @running
+  end
+
+  def get_request(client)
     request_lines = []
     while line = client.gets and !line.chomp.empty?
       request_lines << line.chomp
     end
+    request_lines
+  end
 
-    puts "\nGot this request:"
-    puts request_lines.inspect
-
-    puts "\nSending response."
-    response = "<pre>" + request_lines.join("\n") + "</pre>"
-    output = "<html><head></head><body>#{response}<br>Hello World! #{num_runs/2}</body></html>"
+  def generate_response(request_lines, response)
     headers = ["http/1.1 200 ok",
       "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
       "server: ruby",
       "content-type: text/html; charset=iso-8859-1",
-      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-      client.puts headers
-      client.puts output
+      "content-length: #{response.length}"].join("<br>")
+    output = "<html><head></head><body>#{request_lines}<br><br>#{headers}<br><br>
+    #{response}</body></html>"
+    output
+  end
 
-      puts ["Wrote this response:", headers, output].join("\n")
-      client.close
-      puts "\nResponse complete"
-    end
 end
