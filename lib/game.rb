a3c7f9f76f9587e7de47a5833cdca40b5c025fe2
Ryan Batty <rgbatty@outlook.com>
2016-04-16 21:45:46 -0600

    Final bug fixes for game behavior. Now refuses to take guesses when a game has not been started. Stops game on correct guess. /start_game always starts a new game

--- lib/game.rb
+++ lib/game.rb
@@ -13,6 +13,10 @@ class Game
     @running = false
   end
 
+  def running?
+    @running
+  end
+
   def start_game(client, parsed_request)
     case parsed_request["Verb"]
     when "POST"
@@ -31,6 +35,7 @@ class Game
   end
 
   def game(client, parsed_request)
+    return not_running_response(client, parsed_request) if !running?
     case parsed_request["Verb"]
     when "GET" then game_get(client, parsed_request)
     when "POST" then game_post(client, parsed_request)
@@ -39,7 +44,7 @@ class Game
   end
 
   def game_get(client, parsed_request)
-    get_response(client, parsed_request)
+    running_response(client, parsed_request)
   end
 
   def game_post(client, parsed_request)
@@ -49,8 +54,12 @@ class Game
     post_response(client, parsed_request)
   end
 
+  def not_running_response(client, parsed_request)
+    client.puts "http/1.1 #{ResponseCodes.ok}\r\n\n\r"
+    client.puts "The game is not currently started! Please start it using a post to /start_game!\n\r"
+  end
 
-  def get_response(client, parsed_request)
+  def running_response(client, parsed_request)
     client.puts "http/1.1 #{ResponseCodes.ok}\r\n\n\r"
     client.puts "You've guessed #{@guesses} times\n\r"
     client.puts check_guess
@@ -58,7 +67,6 @@ class Game
 
   def post_response(client, parsed_request)
     client.puts generate_redirect_header
-    # client.puts "Sending Reponse!"
   end
 
   def generate_redirect_header
@@ -74,7 +82,8 @@ class Game
     if invalid_guess?
       response = "Please guess a number between 0 and 100"
     elsif @current_guess == @secret_number
-      response = "You guesses the number! It was #{@secret_number}"
+      @running = false
+      response = "You guessed the number! It was #{@secret_number}. Please use /start_game to play again!"
     elsif @current_guess > @secret_number
       response = "Your guess was too high"
     elsif @current_guess < @secret_number
